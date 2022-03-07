5395f2d307097651d79f987b614f9019870137af
Hanbi Kim <rain7i@naver.com>
2022-03-07 12:45:58 +0900

    2장: 내가 짠 코드

--- movieapp/discountpolicy/DiscountPolicy.java
+++ movieapp/discountpolicy/DiscountPolicy.java
@@ -1,8 +1,20 @@
 package movieapp.discountpolicy;
 
+import movieapp.discountcondition.DiscountConditions;
+
+import java.util.List;
+
 public class DiscountPolicy {
     private PriceDiscountPolicy priceDiscountPolicy;
     private RatioDiscountPolicy ratioDiscountPolicy;
+    private List<DiscountConditions> discountConditionsList;
 
+    public DiscountPolicy(PriceDiscountPolicy priceDiscountPolicy, RatioDiscountPolicy ratioDiscountPolicy) {
+        this.priceDiscountPolicy = priceDiscountPolicy;
+        this.ratioDiscountPolicy = ratioDiscountPolicy;
+    }
 
+    public void setDiscountConditionsList(DiscountConditions ... discountConditions){
+        this.discountConditionsList = List.of(discountConditions);
+    }
 }
