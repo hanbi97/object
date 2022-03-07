030f816b620a65600355d536aee22ff30d88dee5
Hanbi Kim <rain7i@naver.com>
2022-03-07 12:46:30 +0900

    2장: 내가 짠 코드

--- movieapp/discountpolicy/RatioDiscountPolicy.java
+++ movieapp/discountpolicy/RatioDiscountPolicy.java
@@ -2,4 +2,8 @@ package movieapp.discountpolicy;
 
 public class RatioDiscountPolicy {
     private Double discountRatio;
+
+    public RatioDiscountPolicy(Double discountRatio) {
+        this.discountRatio = discountRatio;
+    }
 }
