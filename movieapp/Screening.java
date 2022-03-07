ef2da3ca91636435bb5614bba22ce76ac6af2b80
Hanbi Kim <rain7i@naver.com>
2022-03-07 12:44:48 +0900

    2장: 내가 짠 코드

--- movieapp/Screening.java
+++ movieapp/Screening.java
@@ -1,10 +1,23 @@
 package movieapp;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class Screening {
     private LocalDateTime screeningStart;
     private LocalDateTime screeningEnd;
     private Integer sequenceNumber;
     private Integer runningTime;
+    private List<Booking> bookingList;
+
+    public Screening(LocalDateTime screeningStart, LocalDateTime screeningEnd, Integer sequenceNumber, Integer runningTime) {
+        this.screeningStart = screeningStart;
+        this.screeningEnd = screeningEnd;
+        this.sequenceNumber = sequenceNumber;
+        this.runningTime = runningTime;
+    }
+
+    public void addBooking(Booking booking){
+        bookingList.add(booking);
+    }
 }
