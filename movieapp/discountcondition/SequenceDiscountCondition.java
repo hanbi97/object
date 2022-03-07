99cf74825f23cf0c1a71fadd8a9cff85c03d088c
Hanbi Kim <rain7i@naver.com>
2022-03-07 12:46:47 +0900

    2장: 내가 짠 코드

--- movieapp/discountcondition/SequenceDiscountCondition.java
+++ movieapp/discountcondition/SequenceDiscountCondition.java
@@ -2,4 +2,8 @@ package movieapp.discountcondition;
 
 public class SequenceDiscountCondition {
     private Integer sequenceNumber;
+
+    public SequenceDiscountCondition(Integer sequenceNumber) {
+        this.sequenceNumber = sequenceNumber;
+    }
 }
