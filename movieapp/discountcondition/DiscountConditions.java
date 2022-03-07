f9c4e33f93e498d23d494d98965dc72cc5e6708e
Hanbi Kim <rain7i@naver.com>
2022-03-07 12:47:16 +0900

    2장: 내가 짠 코드

--- movieapp/discountcondition/DiscountConditions.java
+++ movieapp/discountcondition/DiscountConditions.java
@@ -3,4 +3,9 @@ package movieapp.discountcondition;
 public class DiscountConditions {
     private PeriodDiscountCondition periodDiscountCondition;
     private SequenceDiscountCondition sequenceDiscountCondition;
+
+    public DiscountConditions(PeriodDiscountCondition periodDiscountCondition, SequenceDiscountCondition sequenceDiscountCondition) {
+        this.periodDiscountCondition = periodDiscountCondition;
+        this.sequenceDiscountCondition = sequenceDiscountCondition;
+    }
 }
