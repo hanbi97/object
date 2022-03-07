1bf71f3516860b41eb26065fe1889f17b9c1e0dc
Hanbi Kim <rain7i@naver.com>
2022-03-07 12:45:07 +0900

    2장: 내가 짠 코드

--- movieapp/Movie.java
+++ movieapp/Movie.java
@@ -1,6 +1,26 @@
 package movieapp;
 
+import movieapp.discountpolicy.DiscountPolicy;
+
+import java.util.List;
+import java.util.Optional;
+
 public class Movie {
     private String name;
     private Long price;
+    private List<Screening> screeningList;
+    private Optional<DiscountPolicy> discountPolicy;
+
+    public Movie(String name, Long price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public void addScreeningList(Screening screening){
+        screeningList.add(screening);
+    }
+
+    public void addDiscountPolicy(DiscountPolicy discountPolicy){
+        this.discountPolicy= Optional.ofNullable(discountPolicy);
+    }
 }
