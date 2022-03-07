a0d04a11bb50de02e7cd338d88b3868b995aab32
Hanbi Kim <rain7i@naver.com>
2022-03-07 12:46:14 +0900

    2장: 내가 짠 코드

--- movieapp/discountpolicy/PriceDiscountPolicy.java
+++ movieapp/discountpolicy/PriceDiscountPolicy.java
@@ -2,4 +2,8 @@ package movieapp.discountpolicy;
 
 public class PriceDiscountPolicy {
     private Long discountPrice;
+
+    public PriceDiscountPolicy(Long discountPrice) {
+        this.discountPrice = discountPrice;
+    }
 }
